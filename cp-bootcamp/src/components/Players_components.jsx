93ce897cfaef9d5eea2b97b9cdd6b325ed918c8a
WarrenPegg <warren.pegg@hotmail.co.uk>
2026-07-24 18:39:07 +0100

    Updated 'import { supabase }...' line in Players_components.jsx

--- cp-bootcamp/src/components/Players_components.jsx
+++ cp-bootcamp/src/components/Players_components.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "../supabaseClient";
 
 const assetUrl = (file) => `${import.meta.env.BASE_URL}${file}`;
 
