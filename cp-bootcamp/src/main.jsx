1670258216d74e307d1e3f2528a9205e7382ccde
WarrenPegg <warren.pegg@hotmail.co.uk>
2026-07-24 18:23:57 +0100

    Deleted 'import HomePage_LeagueTable from...' in  main.jsx

--- cp-bootcamp/src/main.jsx
+++ cp-bootcamp/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import LeagueTable from "./LeagueTable.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
